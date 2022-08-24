b2ecf8a929579e5bfebeed8297f3c488a0144723
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-08-24 17:49:30 -0600

    fix failed tests

--- src/book.js
+++ src/book.js
@@ -1,11 +1,21 @@
-function createTitle() {
-  
+function createTitle(title) {
+return "The " + title;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+};
+function saveReview(reviews, item) {
+return push()
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
