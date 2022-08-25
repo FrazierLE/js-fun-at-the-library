5b8066681576c03d76e2a9f62d34a7f1e9698380
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-08-25 17:30:30 -0600

    Complete book test

--- src/book.js
+++ src/book.js
@@ -9,14 +9,37 @@ function buildMainCharacter(name, age, pronouns) {
     pronouns: pronouns
   }
 };
-function saveReview(reviews, item) {
-return push()
+
+function saveReview(newReview, review) {
+  if(review.includes(newReview) === false) {
+    return review.push(newReview);}
 }
+
+function calculatePageCount(title, pageCount) {
+  for (var i = 0; i < title.length; i++) {
+    return 20 * title.length
+  }
+}
+
+function writeBook(bookTitle, bookCharacter, pageCount) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: bookTitle.length * 20,
+    genre: 'fantasy'
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
+  return book
+}
+
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
