c6520fac9e445dbf81938992ec72ec55474d1e22
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-08-27 11:15:46 -0600

    complete shelf.js tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,16 +1,46 @@
   function shelfBook(book, shelfGenre) {
-    return shelfGenre.unshift(book)
+    if (shelfGenre.length < 3) {
+    return shelfGenre.unshift(book);
+  }
+}
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+    return shelf.splice(i, 1)
+    }
+  }
+}
+
+
+function listTitles(title) {
+  var titlesOnShelf = []
+  for (var i = 0; i < title.length; i++) {
+    titlesOnShelf.push(title[i].title)
+}
+  return titlesOnShelf.join(', ')
   }
 
-// function saveReview(newReview, review) {
-//   if(review.includes(newReview) === false) {
-//     return review.push(newReview);}
+
+// function searchShelf(book) {
+//   for (var i = 0; i < book.length; i++) {
+//     if (book.some(book.title))
+//       return book.title[i] + true
+//     }
 // }
+function searchShelf(shelfName, bookName) {
+  for (var i = 0; i < shelfName.length; i++) {
+    if (shelfName[i].title === bookName) {
+      return true }
+    }
+    return false
+  }
+
 
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
