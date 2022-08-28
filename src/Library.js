05b4f33e7c8528c5dc71014f6673d4c5dcb51b06
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-08-27 18:32:43 -0600

    Complete tests in library.js file

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,57 @@
 
+function createLibrary(name) {
+var libraryName = {
+  name: name,
+  shelves: {
+    fantasy: [],
+    fiction: [],
+    nonFiction: []}
+  }
+  return libraryName
+}
+
+function addBook(libraryName, book) {
+if (libraryName.shelves.hasOwnProperty('fantasy') === true && book.genre === 'fantasy') {
+libraryName.shelves.fantasy.push(book) }
+if (libraryName.shelves.hasOwnProperty('nonFiction') === true && book.genre === 'nonFiction') {
+libraryName.shelves.nonFiction.push(book) }
+if (libraryName.shelves.hasOwnProperty('fiction') === true && book.genre === 'fiction') {
+libraryName.shelves.fiction.push(book) }
+}
+
+  function checkoutBook(libraryName, book, genre) {
+  var titlesOnShelf = []
+  for (var i = 0; i < libraryName.shelves.fiction.length; i++) {
+    titlesOnShelf.push(libraryName.shelves.fiction[i].title)
+}
+for (var i = 0; i < libraryName.shelves.nonFiction.length; i++) {
+  titlesOnShelf.push(libraryName.shelves.nonFiction[i].title) }
+  for (var i = 0; i < libraryName.shelves.fantasy.length; i++) {
+    titlesOnShelf.push(libraryName.shelves.fantasy[i].title)
+  }
+
+
+  if (genre === 'fiction') { for (var i = 0; i < libraryName.shelves.fiction.length; i++) {
+    if (libraryName.shelves.fiction[i].title === book) {libraryName.shelves.fiction.splice(i, 1)}
+  } return `You have now checked out ${book} from the ${libraryName.name}`}
+
+  else if (genre === 'nonFiction') { for (var i = 0; i < libraryName.shelves.nonFiction.length; i++) {
+    if (libraryName.shelves.nonFiction[i].title === book) {libraryName.shelves.nonFiction.splice(i, 1)}
+    return `You have now checked out ${book} from the ${libraryName.name}`}
+
+  } else if (genre === 'fantasy') { for (var i = 0; i < libraryName.shelves.fantasy.length; i++) {
+    if (libraryName.shelves.fantasy[i].title === book) {libraryName.shelves.fantasy.splice(i, 1)}
+  return `You have now checked out ${book} from the ${libraryName.name}`}
+}
+if(titlesOnShelf.includes(book) !== book) {
+  return `Sorry, there are currently no copies of ${book} available at the ${libraryName.name
+  }`
+}
+}
+
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
